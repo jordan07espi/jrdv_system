f60a4826a8141ad29e4129806aa5db227acf8d55
jordan07espi <jeespinosa5@espe.edu.ec>
2024-11-12 16:25:25 -0500

    Subida inicial de la aplicación de Ionic

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'jrdv',
+  webDir: 'www'
+};
+
+export default config;
